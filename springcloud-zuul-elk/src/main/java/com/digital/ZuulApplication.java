28bd2b3976bc9ee02dc0d6e824bc80b32b88678a
cyc3552637 <395816504@qq.com>
2018-03-09 17:58:45 +0800

    spring cloud use Elasticsearch+Logstach+Kibana by log system

---
+++ springcloud-zuul-elk/src/main/java/com/digital/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+  
+}
